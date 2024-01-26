c438ea4afc2d8e1721e51f3907e1a9f23b11f0a6
Neel <pneel294@gmail.com>
2024-01-25 19:20:46 -0500

    test2

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000
-
 app.get("/api",(req,res)=>{
     res.json({"users" : ["UserOne","UserTwo"]});
 });
 
-// app.listen(port, () => {
-//     console.log("App is running at port " + port);
-//   });
+app.listen(port, () => {
+    console.log("App is running at port " + port);
+  });
