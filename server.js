7ac2f9041106e590cfd29d54dfe35ddc17626d29
Neel <pneel294@gmail.com>
2024-01-25 19:12:32 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require('./db/dbinit.js');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000
+
+app.get("/api",(req,res)=>{
+    res.json({"users" : ["UserOne","UserTwo"]});
+});
+
+app.listen(port, () => {
+    console.log("App is running at port " + port);
+  });
