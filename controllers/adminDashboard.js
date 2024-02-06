a694edb9459902daf51ac3eee0f2d4122abd5528
Neel <pneel294@gmail.com>
2024-02-06 14:51:29 -0500

    commit

--- controllers/adminDashboard.js
+++ controllers/adminDashboard.js
@@ -1,20 +1,23 @@
 const userModel = require("../models/user.js");
 
 const adminDashboard = async(req,res)=>{
-
-    const users = await userModel.find({});
-    console.log(users);
-    let allUsers = [];
-    for(i=0;i<users.length;i++ ){
-        const userObject={
-            fullName: users[i].firstName + ' ' + users[i].lastName,
-            userType: users[i].userType,
-            specialization: users[i].specialization 
+    try{
+        const users = await userModel.find({});
+        console.log(users);
+        let allUsers = [];
+        for(i=0;i<users.length;i++ ){
+            const userObject={
+                fullName: users[i].firstName + ' ' + users[i].lastName,
+                userType: users[i].userType,
+                specialization: users[i].specialization 
+            }
+            allUsers.push(userObject);
         }
-        allUsers.push(userObject);
+        
+        res.json(allUsers);
+    }catch(e){
+        console.error(e);
     }
-    
-    res.json(allUsers);
 
     
 
