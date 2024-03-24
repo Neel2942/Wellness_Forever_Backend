098b65090bcd3c0a4baf93379d0dcb688f70c883
Neel <pneel294@gmail.com>
2024-03-24 16:34:59 -0400

    added admin appointment list

--- controllers/adminAppointmentList.js
+++ controllers/adminAppointmentList.js
@@ -4,21 +4,27 @@ const userModel = require("../models/user.js");
 const adminAppointmentList = async(req,res) => {
 
     const appointmentList = await bookAppointmentModel.find();
-
+    const noOfAppointments = await bookAppointmentModel.find({doctorId:appointmentList.doctorId});
     let userAppointmentList = [];
 
     for(i=0;i<appointmentList.length;i++){
         let patientData = await userModel.findById(appointmentList[i].patientId);
        let doctorData = await userModel.findById(appointmentList[i].doctorId);
        appointmentListData = {
-         patientName: patientData.firstName + " " + patientData.lastName,
          doctorName: "Dr" + " " +doctorData.firstName + " " + doctorData.lastName,
+         noOfAppointments: noOfAppointments.length,
          time: appointmentList[i].time,
          date: appointmentList[i].date,
+         listOfPatients:[{
+            patientName: patientData.firstName + " " + patientData.lastName,
+            date:appointmentList[i].date,
+            time:appointmentList[i].time,
+            symptoms:appointmentList[i].symptoms
+         }
+         ]
        },
        userAppointmentList.push(appointmentListData);
     }
-
     res.send(userAppointmentList);
 }
 
