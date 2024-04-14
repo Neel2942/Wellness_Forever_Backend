d38556a65957086440c855ad0ad8e8063fabcb59
Zaid Alam <zaidalam0731@gmail.com>
2024-04-14 19:28:29 -0400

    Change in suthenticarion

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,11 @@
 const userModel = require("../models/user");
 
 const authMiddleware = async(req,res,next)=>{
-    const user = await userModel.findOne({
-        _id: loggedIn,
-      });
-      if (user == null) {
-        res.json("notLoggedIn");
-      }else{
-        next();
-      }
+  if (!req.session.userId) {
+    console.log("User", req.session.userId);
+    return res.json("notLoggedIn");
+  }
+  next();
 }
 
 module.exports = authMiddleware
