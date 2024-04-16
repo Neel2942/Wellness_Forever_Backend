334bf141bbaf40135804d95034cadd8192da3596
Neel <pneel294@gmail.com>
2024-04-16 19:00:34 -0400

    updates

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -17,7 +17,7 @@ const userLogin = async (req, res) => {
       );
       if (paswordCheck) {
         const sessionId = await createSession(check._id);
-        console.log(sessionId);
+        console.log(sessionId.userId);
         const user = await userModel.findOne({ _id: check._id });
         const userData = [
           {
