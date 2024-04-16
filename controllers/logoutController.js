0637c89314dfd59801c78efc1a20e9b52b986ae6
Neel <pneel294@gmail.com>
2024-04-16 19:25:38 -0400

    updates

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,18 +1,21 @@
-const {getUserIdFromSession,deleteSession} = require('../models/session.js');
+const {sessionModel,getUserIdFromSession,deleteSession} = require('../models/session.js');
 
 const logoutController = async(req, res) => {
-  const sessionUserId = await getUserIdFromSession();
-  if(sessionUserId){
-    // Destroy the session to logout the user
-    deleteSession(sessionUserId);
-    req.session.destroy((err) => {
-      if (err) {
-        console.error("Logout error:", err);
-        return res.status(500).json({ message: "Error logging out" });
-      } else {
-        return res.json({ message: "Logout Successfull" });
-      }
-    });
+  const userId = await sessionModel.find();
+  if(userId){
+    const sessionUserId = await getUserIdFromSession(userId[0]);
+    if(sessionUserId){
+      // Destroy the session to logout the user
+      deleteSession(sessionUserId);
+      req.session.destroy((err) => {
+        if (err) {
+          console.error("Logout error:", err);
+          return res.status(500).json({ message: "Error logging out" });
+        } else {
+          return res.json({ message: "Logout Successfull" });
+        }
+      });
+    }
   }
   }
   
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,15 @@
-const {getUserIdFromSession} = require("../models/session.js");
+const {sessionModel,getUserIdFromSession} = require("../models/session.js");
 
 const authMiddleware = async(req,res,next)=>{
-  const userId = req.session.userId;
-  console.log(userId);
-  const sessionUserId = await getUserIdFromSession();
-  if (sessionUserId) {
-    next();
-  }else{
-    console.log("User", sessionUserId);
-    return res.json("notLoggedIn");
+  const userId = await sessionModel.find();
+  if(userId){
+    const sessionUserId = await getUserIdFromSession(userId[0]);
+    if (sessionUserId) {
+      next();
+    }else{
+      console.log("User", sessionUserId);
+      return res.json("notLoggedIn");
+    }
   }
 }
 
